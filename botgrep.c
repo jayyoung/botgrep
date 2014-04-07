be7e9ce7836205b70dd2ab6813ce2dd85e167050
Jay <jxy@dynamic200-213.cs.bham.ac.uk>
2014-04-07 16:23:37 +0100

    C++ Port

--- botgrep.c
+++ botgrep.c
@@ -1,24 +1,22 @@
-#include <stdio.h>	
+#include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
+#include <iostream>
 
 int main() {
 
 	int complexity_of_botgrep = rand()%10;
 	int success_of_botgrep = 0;
+	time_t* JOE_FINISH_PHD_DATE = NULL;
 
 	printf("--- preparing to grep bots ---\n");
-	srand(time(NULL));
+	srand(time(JOE_FINISH_PHD_DATE));
 
 	sleep(rand()%3);
-	
-	for(int i = 0; i < complexity_of_botgrep; i++) {
-		printf("Grepping...\n");
-	}
 
-	sleep(rand()%3);
-	
+		sleep(rand()%3);
+
 		printf("*** Calculating graph-matching matrix...\n");
 
 		sleep(rand()%4);
@@ -28,7 +26,7 @@ int main() {
 		sleep(rand()%9);
 
 		printf("*** Applying principle components analysis\n");
-	
+
 		sleep(rand()%8);
 
 		printf("*** Evaluating model on itself\n");
@@ -37,10 +35,16 @@ int main() {
 
 		printf("*** Improving random walks...\n");
 
-		sleep(rand()%12);		
+		sleep(rand()%12);
 
-		printf("Bots successfully grepped: %d\n",success_of_botgrep );
-	
-	
+	for(int i = 0; i < complexity_of_botgrep; i++) {
+			std::cout << "Grepping..." << std::endl;
+			sleep(rand()%3);
+	}
+
+	printf("Bots successfully grepped: %d\n",success_of_botgrep );
+
+
+
 	return 0;
 }

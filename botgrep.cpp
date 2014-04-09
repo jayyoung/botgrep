ff54591ae36b3696c6aedf5382dc4ab7f0e80047
Jay <jxy@dynamic200-213.cs.bham.ac.uk>
2014-04-09 15:40:07 +0100

    code style optimisations

--- botgrep.cpp
+++ botgrep.cpp
@@ -13,39 +13,40 @@ int main() {
 	int success_of_botgrep = 0;
 	time_t* JOE_FINISH_PHD_DATE = NULL;
 
-	std::cout << "--- preparing to grep bots ---\n";
+std::cout << "--- preparing to grep bots ---\n";
 	srand(time(JOE_FINISH_PHD_DATE));
-		sleep(rand()%3);
-		std::cout << "*** Calculating graph-matching matrix...\n";
-		sleep(rand()%4);
-		std::cout << "*** Determining machine learning paradigm\n";
-		sleep(rand()%9);
-		std::cout << "*** Applying principle components analysis\n";
-		sleep(rand()%8);
-		std::cout << "*** Evaluating model on itself\n";
-		sleep(rand()%6);
-		std::cout << "*** Improving random walks...\n";
-		sleep(rand()%12);
+
+				sleep(rand()%3);
+std::cout << "*** Calculating graph-matching matrix...\n";
+				sleep(rand()%4);
+std::cout << "*** Determining machine learning paradigm\n";
+				sleep(rand()%9);
+std::cout << "*** Applying principle components analysis\n";
+				sleep(rand()%8);
+std::cout << "*** Evaluating model on itself\n";
+				sleep(rand()%6);
+std::cout << "*** Improving random walks...\n";
+				sleep(rand()%12);
 
 while(complexity_of_botgrep > 0) {
 	for(int i = 0; i < complexity_of_botgrep*2; i++) {
 				for(int k = 0; k < i; k++) {
 					std::cout << "\t";
 				}
-			std::cout << "Grepping..." << std::endl;
+std::cout << "Grepping..." << std::endl;
 			usleep(55000);
 	}
 	for(int i = complexity_of_botgrep*2; i >= 0; i--) {
 				for(int k = 0; k < i; k++) {
 					std::cout << "\t";
 				}
-			std::cout << "Grepping..." << std::endl;
+std::cout << "Grepping..." << std::endl;
 			usleep(55000);
 	}
 	complexity_of_botgrep--;
 }
 
-	std::cout << "Bots successfully grepped: " << success_of_botgrep << std::endl;
+std::cout << "Bots successfully grepped: " << success_of_botgrep << std::endl;
 
 
 
